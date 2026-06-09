63c7595bc3c346710fa458ab73aabc6b64686ccb
IameRa <jonanoefinley@gmail.com>
2026-06-09 13:34:41 +0200

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const ALLOWED_ORIGINS = [
     'http://localhost:5500',
     'http://127.0.0.1:5500',
     'http://localhost:3000',
+    'https://iamera.github.io',
     'https://iamera.github.io/OnePlan'
 ];
 
