f84718217152dfff70480a51dcf9ecae6de760e2
IameRa <jonanoefinley@gmail.com>
2026-08-05 10:43:01 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // OnePlan Service Worker
-const CACHE_VERSION = 'oneplan-v22';
+const CACHE_VERSION = 'oneplan-v23';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
